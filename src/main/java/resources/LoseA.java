82c6d259e7ccaf6be946d1ce7658337d573d2d7a
Naoya Nitta <n-nitta@konan-u.ac.jp>
2022-08-12 16:08:43 +0900

    Initial commit.

---
+++ src/main/java/resources/LoseA.java
@@ -0,0 +1,26 @@
+package resources;
+
+import values.Card;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class LoseA {
+    HandsA handsA;
+
+    public LoseA(HandsA handsA) {
+        this.handsA = handsA;
+    }
+
+    public boolean getValue() {
+        boolean result = true;
+        for (Card card : this.handsA.getValue()) {
+            if (!card.isFace()) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
